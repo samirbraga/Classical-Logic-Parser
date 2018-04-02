01131068eccc9721d4f09a070bf699b0d529d449
samirbraga <samirchavess@gmail.com>
2018-04-02 20:28:08 -0300

    Adding Lexer class methods

--- lexer.js
+++ lexer.js
@@ -1,4 +1,25 @@
 class Lexer {
+    constructor(text) {
+        this.text = text;
+        this.pos = 0;
+        this.currentChar = this.text[this.pos];
+    }
+
+    advance() {
+        if (this.pos < this.text.length - 1 && this.currentChar != null) {
+            this.currentChar = this.text[++this.pos];
+        }
+    }
+
+    skipWhiteSpace() {
+        while (this.currentChar != null && this.currentChar == ' ') {
+            this.advance()
+        }
+    }
+
+    atomic() {
+        
+    }
 }
 
 module.exports = Lexer;
